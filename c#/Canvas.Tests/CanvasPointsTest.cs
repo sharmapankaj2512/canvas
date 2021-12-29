73f1117be981e1baae2db637ed433523318b7adc
Pankaj Sharma <sharmapankaj2512@gmail.com>
2021-12-29 12:03:39 +0100

    :recycle: Rename test and make it parameterized

---
+++ c#/Canvas.Tests/CanvasPointsTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Canvas.Tests;
+
+[TestFixture]
+public class CanvasPointsTest
+{
+    public static IEnumerable<TestCaseData> CanvasPointsData
+    {
+        get
+        {
+            yield return new TestCaseData(0, 0, Enumerable.Empty<Point2D>()).SetName("empty canvas zero points");
+            yield return new TestCaseData(1, 1, new List<Point2D> {new(0, 0)}).SetName(
+                "one by one canvas has one point");
+        }
+    }
+
+
+    [TestCaseSource("CanvasPointsData")]
+    public void CanvasPoints(int width, int height, IEnumerable<Point2D> expected)
+    {
+        var aCanvas = Canvas.CreateCanvas(width, height);
+        Assert.AreEqual(aCanvas.Points(), expected);
+    }
+}
