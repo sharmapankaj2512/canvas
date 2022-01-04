a49c52a36191d05b163f37cd09f3e6c87bed4531
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-04 09:09:00 +0100

    :test_tube: FAIL command source create canvas test

---
+++ c#/Canvas.Tests/ICommandSourceTest.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Canvas.Tests;
+
+[TestFixture]
+public class CommandSourceTest
+{
+    [Test]
+    public void CreateCanvasCommand()
+    {
+        var source = new ConsoleCommandSource();
+
+        Assert.AreEqual(true, source.MoveNext());
+        Assert.AreEqual(new CreateCanvas(0, 0), source.Current);
+    }
+}
+
+public class ConsoleCommandSource : ICommandSource
+{
+    public bool MoveNext()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICommand Current { get; }
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<CreateCanvas> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
