83203d99c85664627e108077562a25e83491619e
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-04 11:57:35 +0100

    :test_tube: FAIL command source draw point test

--- c#/Canvas.Tests/ICommandSourceTest.cs
+++ c#/Canvas.Tests/ICommandSourceTest.cs
@@ -25,9 +25,20 @@ public class CommandSourceTest
         var reader = new StringReader("create 0 0\nprint\n");
         var source = new ConsoleCommandSource(reader);
         var _ = source.Current; // ignore create
-        
+
         Assert.AreEqual(true, source.MoveNext());
         Assert.AreEqual(new PrintCanvas(), source.Current);
+    }
+
+    [Test]
+    public void DrawPointCommand()
+    {
+        var reader = new StringReader("create 1 1\npoint 0 0\n");
+        var source = new ConsoleCommandSource(reader);
+        var _ = source.Current; // ignore create
+
+        Assert.AreEqual(true, source.MoveNext());
+        Assert.AreEqual(new DrawPoint(0, 0), source.Current);
     }
 }
 
