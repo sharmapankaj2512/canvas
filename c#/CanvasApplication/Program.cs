8bdeab266685be6120c6d4f9438d5e647d80cbc3
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-06 15:42:49 +0100

    :memo: create a canvas console application

---
+++ c#/CanvasApplication/Program.cs
@@ -0,0 +1,7 @@
+﻿using Canvas;
+using Canvas.Tests;
+
+var source = new ConsoleCommandSource(Console.In);
+var display = new ConsoleDisplay(Console.Out);
+var repl = new Repl(source, display);
+repl.Start();
