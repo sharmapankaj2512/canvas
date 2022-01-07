92992811135d1654ebde315a30cc07fbe7cf5927
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-07 22:27:07 +0100

    :white_check_mark: PASS canvas one by one points test

--- f#/canvas/Canvas.Tests/PointsTest.fs
+++ f#/canvas/Canvas.Tests/PointsTest.fs
@@ -1,7 +1,12 @@
 module Canvas.Tests.PointsTest
 
+open Canvas.Tests.ReplTest
 open NUnit.Framework
 
 [<Test>]
 let ZeroByZero() =
-    Assert.AreEqual(Set.empty, ReplTest.points(0, 0))
+    Assert.AreEqual(Set.empty, points(0, 0))
+    
+[<Test>]
+let OneByOneCanvas() =
+    Assert.AreEqual(set [Point2D(0, 0)], points(1, 1))
--- f#/canvas/Canvas.Tests/ReplTest.fs
+++ f#/canvas/Canvas.Tests/ReplTest.fs
@@ -5,13 +5,14 @@ open NUnit.Framework
 
 type Command = CreateCanvas of Width: int * Height: int
 
-type Point2D = { X: int; Y: int }
+type Point = Point2D of X: int * Y: int
 type CommandSource = unit -> Command
-type Display = Set<Point2D> -> unit
+type Display = Set<Point> -> unit
 
 type Repl = CommandSource -> Display -> unit -> unit
 
-let points = fun (width, height) -> set []
+let points = fun (width: int, height: int) ->
+    if (width = 0 && height = 0) then set [] else set [Point2D(0, 0)]
 
 let repl: Repl =
     fun commandSource display ->
@@ -23,7 +24,7 @@ let Setup () = ()
 
 [<Test>]
 let CreateCanvasCommand () =
-    let commandSource = fun () -> CreateCanvas(Width = 0, Height = 0)
-    let display = fun points -> Assert.AreEqual(List<Point2D>.Empty, points)
+    let commandSource =fun () -> CreateCanvas(Width = 0, Height = 0)
+    let display =fun points -> Assert.AreEqual(List<Point>.Empty, points)
     let start = repl commandSource display
     start ()
