ae12c627d32ff3c446e75c79cf14b28925bddc59
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-07 22:04:17 +0100

    :recycle: REFACTOR extract points function

--- f#/canvas/Canvas.Tests/ReplTest.fs
+++ f#/canvas/Canvas.Tests/ReplTest.fs
@@ -3,26 +3,27 @@ module Canvas.Tests.ReplTest
 open Microsoft.FSharp.Core
 open NUnit.Framework
 
-type CreateCanvas = { Width: int; Height: int }
+type Command = CreateCanvas of Width: int * Height: int
 
 type Point2D = { X: int; Y: int }
-type CommandSource = unit -> CreateCanvas
+type CommandSource = unit -> Command
 type Display = Point2D list -> unit
 
 type Repl = CommandSource -> Display -> unit -> unit
 
+let points = fun (width, height) -> List<Point2D>.Empty
+
 let repl: Repl =
-    fun commandSource display -> fun () -> display List<Point2D>.Empty
-
-
+    fun commandSource display ->
+        match commandSource () with
+        | CreateCanvas (w, h) -> fun () -> display (points (w, h))
 
 [<SetUp>]
 let Setup () = ()
 
 [<Test>]
 let CreateCanvasCommand () =
-    let commandSource = fun () -> { Width = 0; Height = 0 }
-    let display =
-        fun points -> Assert.AreEqual(List<Point2D>.Empty, points)
+    let commandSource = fun () -> CreateCanvas(Width = 0, Height = 0)
+    let display = fun points -> Assert.AreEqual(List<Point2D>.Empty, points)
     let start = repl commandSource display
     start ()
