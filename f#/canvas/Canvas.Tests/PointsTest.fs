830ea860447f6e4542e03903f8fc5e6087ad41cb
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-07 22:29:29 +0100

    :test_tube: FAIL one by two canvas points test

--- f#/canvas/Canvas.Tests/PointsTest.fs
+++ f#/canvas/Canvas.Tests/PointsTest.fs
@@ -10,3 +10,7 @@ let ZeroByZero() =
 [<Test>]
 let OneByOneCanvas() =
     Assert.AreEqual(set [Point2D(0, 0)], points(1, 1))
+    
+[<Test>]
+let OneByTwoCanvas() =
+    Assert.AreEqual(set [Point2D(0, 0), Point2D(0, 1)], points(1, 2))
