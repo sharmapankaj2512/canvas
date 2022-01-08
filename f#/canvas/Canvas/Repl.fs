4d7f9788837209b59f1dc992ee3461d4ad43f091
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-08 20:14:20 +0100

    :recycle: REFACTOR extract handle function

--- f#/canvas/Canvas/Repl.fs
+++ f#/canvas/Canvas/Repl.fs
@@ -3,11 +3,11 @@ namespace Canvas
 open Canvas
 
 module Repl =
+    let handle (display: Display) (command: Command) =
+        match command with
+        | CreateCanvas (w, h) -> display.Display(points (w, h))
+        | Quit -> display.Display("Good bye!")
+
     let repl: Repl =
-        fun commandSource display ->            
-                commandSource ()
-                |> Seq.iter
-                    (fun command ->
-                        match command with
-                        | CreateCanvas (w, h) -> display.Display(points (w, h))
-                        | Quit -> display.Display("Good bye!"))
+        fun commandSource display ->
+            commandSource () |> Seq.iter (handle display)
