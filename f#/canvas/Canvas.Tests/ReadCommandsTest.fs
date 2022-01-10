66d4069c1444dc436d77c3d9746f4d9b121ebf48
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-01-10 16:22:18 +0100

    :white_check_mark: PASS parse multiple commands

--- f#/canvas/Canvas.Tests/ReadCommandsTest.fs
+++ f#/canvas/Canvas.Tests/ReadCommandsTest.fs
@@ -7,20 +7,26 @@ open NUnit.Framework.Internal
 
 module ReadCommandsTest =
 
-    let consoleCommandSource (reader: TextReader) () : Command =
-        let line = reader.ReadLine().Trim()
-        let tokens = line.Split(" ")
-        if (tokens[0].StartsWith("create") && tokens.Length >= 3) then            
-            let width = tokens[1] |> int
-            let height = tokens[2] |> int
-            CreateCanvas(width, height)
-        else
-            Quit
+    let consoleCommandSource (reader: TextReader) () : seq<Command> =
+        seq {
+            let line = reader.ReadLine()
+            if (line = null) then
+                ()
+            else
+                let tokens = line.Trim().Split(" ")
+                if (tokens[0].StartsWith("create") && tokens.Length >= 3) then
+                    let width = tokens[1] |> int
+                    let height = tokens[2] |> int
+                    yield CreateCanvas(width, height)
+                else
+                    yield Quit
+        }
+
 
     [<Test>]
     let parseQuitCommand () =
         let reader = new StringReader("quit\n")
-        Assert.AreEqual(Quit, consoleCommandSource reader ())
+        Assert.AreEqual(Quit, Seq.head(consoleCommandSource reader ()))
 
     [<Test>]
     [<TestCase("create 0 0\n", 0, 0)>]
@@ -29,25 +35,27 @@ module ReadCommandsTest =
     [<TestCase(" create 0 0\n", 0, 0)>]
     let parseCreateCanvasCommand (line: string, width: int, height: int) =
         let reader = new StringReader(line)
-        Assert.AreEqual(CreateCanvas(width, height), consoleCommandSource reader ())
+        Assert.AreEqual(CreateCanvas(width, height), Seq.head(consoleCommandSource reader ()))
 
     [<Test>]
     let parseCreateCanvasIgnoreExcessParameters () =
         let reader = new StringReader("create 0 1 2")
-        Assert.AreEqual(CreateCanvas(0, 1), consoleCommandSource reader ())
+        Assert.AreEqual(CreateCanvas(0, 1), Seq.head(consoleCommandSource reader ()))
         
     [<Test>]
     let parseCreateCommandWithFewerParameters () =
         let reader = new StringReader("create 0 ")
-        Assert.AreEqual(Quit, consoleCommandSource reader ())
+        Assert.AreEqual(Quit, Seq.head(consoleCommandSource reader ()))
         
     [<Test>]
     let parseUnrecognizedCommand () =
         let reader =
             new StringReader("### invalid command ###")
 
-        Assert.AreEqual(Quit, consoleCommandSource reader ())
+        Assert.AreEqual(Quit, Seq.head(consoleCommandSource reader ()))
 
-//    [<Test>]
-//    [<Ignore("")>]
-//    let parseMultipleCommands = ignore
+    [<Test>]    
+    let parseMultipleCommands () =
+        let reader = new StringReader("create 0 0\n1create 0 0\n")
+        for command in (consoleCommandSource reader ()) do
+            Assert.AreEqual(CreateCanvas(0, 0), command)
